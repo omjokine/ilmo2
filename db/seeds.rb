2da1060dc440446fdd073656efb35bbec0af88bb
olli <olli@82-203-227-90.dsl.gohome.fi>
2009-11-12 22:59:26 +0200

    seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #   
 #   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
 #   Major.create(:name => 'Daley', :city => cities.first)
+
+overwrite = ENV['overwrite'] == "yes"
+how_many_courses = ENV['courses'].to_i
+
+puts "Usage: rake db:seed courses=<number of courses> <overwrite=true>" if how_many_courses <= 0 # to_i returns 0 if ENV['courses'] is empty
+
+Course.delete_all if overwrite
+ 
+for i in 1..how_many_courses
+  Course.create(:name => "#{'Course #' + i.to_s}", :description => "Non eram nescius, Brute, cum, quae summis ingeniis exquisitaque doctrina philosophi Graeco sermone tractavissent, ea Latinis litteris mandaremus, fore ut hic noster labor in varias reprehensiones incurreret. nam quibusdam, et iis quidem non admodum indoctis, totum hoc displicet philosophari")
+end
